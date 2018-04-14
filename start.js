0f2db5daad1017f53ad206133d1b6c855d875188
harikarodda <harikarodda@gmail.com>
2018-04-14 02:06:58 -0500

    third commit

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const path = require('path');
 var app =express();
 const port = process.env.PORT || 3000;
 
@@ -8,9 +8,10 @@ const port = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
 
 //res.send('<h1>Hello!</h1>');
-res.send('index.html');
+res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.listen(port,()=>
 {
-  console.log(`server running on ${port}`)
+  console.log(`server running on ${port}`);
 });
